a8fce7842ea6abf6790e3615eb6ccd4a3e2b3fa9
Marco <maguilar@patriotsoftware.com>
2024-10-14 11:19:56 -0400

    test

--- gha/expired-branches.sh
+++ gha/expired-branches.sh
@@ -5,8 +5,8 @@ formatted_exp_date=$(date -d @${EXPIRATION_DATE} +'%Y-%m-%d %H:%M:%S')
 printf '%s\n' "Branches with last commit older than $formatted_exp_date are expired. Resources can be restored with another branch deploy."
 
 # GitHub branches excluding master/main without origin prefix
-git fetch --all
-github_branches=$(git branch -r | grep -vE 'origin/(main|master)' | sed 's/origin\///')
+git_remote="https://github.com/${repository}.git"
+github_branches=$(git ls-remote --heads $git_remote | awk '{print $2}' | sed 's/refs\/heads\///' | grep -vE '(main|master)')
 
 # Helm installed branches excluding main 
 #   NOTE: custom colum is using branch name used during install
@@ -15,7 +15,8 @@ expired_branches=()
 
 # Expired GitHub branches
 for branch in ${github_branches[@]}; do    
-    LAST_COMMIT=$(git log -1 --format=%ct "origin/$branch")
+    COMMIT_HASH=$(git ls-remote $git_remote refs/heads/$branch | awk '{print $1}')
+    LAST_COMMIT=$(git show -s --format=%ct $COMMIT_HASH)
 
     if [ $LAST_COMMIT -lt $EXPIRATION_DATE ] && [[ $branch != v* ]]; then
         LAST_COMMIT_DATE=$(date -d @$LAST_COMMIT +'%Y-%m-%d %H:%M:%S')
@@ -47,11 +48,11 @@ fi
 printf '\n%s\n' "//// GitHub Branches ////"
 printf '%s\n' ${github_branches[@]}
 
-printf '\n%s\n' "//// Helm branches ////"
+printf '\n%s\n' "//// Helm Branches ////"
 printf '%s\n' ${helm_branches[@]}
 
 printf '\n%s\n' "//// Expired Branches ////"
 printf '%s\n' ${expired_branches[@]}
 expired_data=$(printf '"%s"\n' ${expired_branches[@]}|paste -sd, -)
 json_expired_data="{\"branch_name\": [$expired_data]}"
-echo "expired-json=$json_expired_data" >> $GITHUB_OUTPUT
+echo "expired-branches-json=$json_expired_data" >> $GITHUB_OUTPUT
