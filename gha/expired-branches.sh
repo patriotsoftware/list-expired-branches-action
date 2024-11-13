8b887262540fbaa8efca5d356c2488b1ea42f8ba
Marco <maguilar@patriotsoftware.com>
2024-11-13 13:57:58 -0500

    test

--- gha/expired-branches.sh
+++ gha/expired-branches.sh
@@ -20,7 +20,7 @@ fi
 github_branches=$(git ls-remote --heads $git_remote | awk '{print $2}' | sed 's/refs\/heads\///' | grep -vE '(main|master)')
 
 printf '\n%s\n' "//// GitHub Branches ////"
-printf '"%s"\n' ${github_branches[@]}
+printf '%s\n' ${github_branches[@]}
 
 # Helm installed branches excluding main 
 #   NOTE: custom column is using branch name used during install
@@ -29,6 +29,7 @@ printf '\n%s\n' "//// Helm Branches ////"
 printf '%s\n' ${helm_branches[@]}
 
 expired_branches=()
+printf '\n%s\n' "//// Determin Expiration Dates ////"
 
 # Expired GitHub branches
 for branch in ${github_branches[@]}; do
