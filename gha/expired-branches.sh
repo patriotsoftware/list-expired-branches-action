3c5c5571326c92eb091f17329a0af204052ab9a9
Marco <maguilar@patriotsoftware.com>
2024-11-13 13:50:28 -0500

    test

--- gha/expired-branches.sh
+++ gha/expired-branches.sh
@@ -20,13 +20,13 @@ fi
 github_branches=$(git ls-remote --heads $git_remote | awk '{print $2}' | sed 's/refs\/heads\///' | grep -vE '(main|master)')
 
 printf '\n%s\n' "//// GitHub Branches ////"
-printf '%s\n' "${github_branches[@]}"
+printf '"%s"\n' "${github_branches[@]}"
 
 # Helm installed branches excluding main 
 #   NOTE: custom column is using branch name used during install
 helm_branches=$(kubectl get deploy -n $namespace --no-headers  -o custom-columns='BRANCH:.spec.template.spec.containers[*].env[?(@.name=="BRANCH_NAME")].value' | tr ' ' '\n' | sort -u | grep -v -E '^(main|master)$')
 printf '\n%s\n' "//// Helm Branches ////"
-printf '%s\n' "${helm_branches[@]}"
+printf '"%s"\n' "${helm_branches[@]}"
 
 expired_branches=()
 
@@ -48,9 +48,9 @@ done
 for h_branch in "${helm_branches[@]}"; do
     on_GitHub='false'
     for gh_branch in "${github_branches[@]}"; do 
-        [[ $gh_branch == $h_branch ]] && echo "$h_branch is on GitHub"  && on_GitHub='true' && break
+        [[ $gh_branch == $h_branch ]] && echo "'$h_branch' is on GitHub" && on_GitHub='true' && break
     done 
-    [[ $on_GitHub == 'false' ]] && echo "$h_branch not found on GitHub"  && expired_branches+=($h_branch)
+    [[ $on_GitHub == 'false' ]] && echo "'$h_branch' not found on GitHub" && expired_branches+=($h_branch)
 done
 
 if [ -z $expired_branches ]; then
@@ -63,7 +63,7 @@ fi
 
 # display results
 printf '\n%s\n' "//// Expired Branches ////"
-printf '%s\n' "${expired_branches[@]}"
+printf '"%s"\n' "${expired_branches[@]}"
 
 expired_data=$(printf '"%s"\n' "${expired_branches[@]}"|paste -sd, -)
 json_expired_data="{\"branch_name\": [$expired_data]}"
